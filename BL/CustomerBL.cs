5704333c49321d16208f6b6757a1d7fea7cb65de
ThomasMoerschel <tj.moerschel@gmail.com>
2021-07-02 21:25:46 -0400

    BL and DL Detailed Functionality Annotation

--- BL/CustomerBL.cs
+++ BL/CustomerBL.cs
@@ -7,15 +7,16 @@ namespace StoreApp
     public class CustomerBL : ICustomerBL
     {
         private IRepository _repo;
+        //Referenced by the ShowCustomerMenu(...) and AddCustomerMenu(...) within the MenuFactory takes in a IRepository variable (_repo)
         public CustomerBL(IRepository p_repo){
             _repo = p_repo;
         }
-
+        //Irepository field variable calls the repository AddCustomer() method which inputs an object to be added to the collection
         public Customer AddCustomer(Customer p_customer)
         {
             return _repo.AddCustomer(p_customer);
         }
-
+        //Irepository field variable that calls the GetAllCustoms() method defined within the Repository class
         public List<Customer> GetAllCustomers()
         {
             return _repo.GetAllCustomers();
--- DL/Repository.cs
+++ DL/Repository.cs
@@ -5,18 +5,26 @@ using System.Text.Json;
 
 namespace StoreApp
 {
+    //manages the submission of objects to a .json file
     public class Repository : IRepository
     {
+        //denotes file path of where the .json file is located 
         private const string _filePath = "./../DL/Database/Customer.json";
+        //creates a private string that is Serializes the list of customers
         private string _jsonString;
         
+        //Called within the BL from _rep, a repository field variable, takes in customer object
         public Customer AddCustomer(Customer p_customer)
         {
+            //instantiates a list of customer to the GetAllCustomers() method below where the JSON file is read and committed to the list
             List<Customer> listOfCustomers = this.GetAllCustomers();
+            //After adding the preexisting json list, add the next object (p_customer) and rewrite JSON file 
             listOfCustomers.Add(p_customer);
-
+            //serializes the list of customers and sets it equal to the _jsonString
             _jsonString = JsonSerializer.Serialize(listOfCustomers, new JsonSerializerOptions{WriteIndented = true});
+            //Writes the _jsonString to _filePath
             File.WriteAllText(_filePath, _jsonString);
+            //returns the customer object
             return p_customer;
         }
 
@@ -24,13 +32,14 @@ namespace StoreApp
         {
             try
             {
+                //reads test within the json file at the _filepath and saves it to the private _jsonString
                 _jsonString = File.ReadAllText(_filePath);
             }
             catch (System.Exception)
             {
                 throw new Exception ("File path is invalid");
             }
-                //This will return a list of Customer from the jsonString that it came from
+            //This will return a list of Customer from the jsonString that it came from
             return JsonSerializer.Deserialize<List<Customer>>(_jsonString);
         }
 
